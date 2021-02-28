9ac0ffb143ea94c980b23839a8bdd9e9f35f6b16
Lewis Patten <lp921@york.ac.uk>
2021-02-28 19:18:43 +0000

    Added allot

--- model.py
+++ model.py
@@ -2,9 +2,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class AlexNet(nn.Module):
+    """ A implementation of the AlexNet architecture from the paper 'ImageNet 
+        Classification with Deep Convolutional Neural Networks' """
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=5, padding_mode='reflect')
+        # The paper specifies that this layer should be stride 4 however this does not work with 
+        # the input dimensions, I work under the assumption that the definition of stride used 
+        # in the Alexnet paper and the pytorch library differ.
+        self.conv1 = nn.Conv2d(3, 96, kernel_size=11, stride=2, padding=5, padding_mode='reflect') 
         self.conv2 = nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2, padding_mode='reflect')
         self.conv3 = nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
         self.conv4 = nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
@@ -17,9 +22,7 @@ class AlexNet(nn.Module):
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
-        print(x.shape)
         x = self.pool(F.relu(self.conv2(x)))
-        print(x.shape)
         x = F.relu(self.conv3(x))
         x = F.relu(self.conv4(x))
         x = self.pool(F.relu(self.conv5(x)))
--- train.py
+++ train.py
@@ -2,10 +2,14 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
+from torch.utils.data import random_split
 from torchvision import datasets
 from torchvision import transforms
+import numpy as np
 from model import AlexNet
 
+# Select device to train on
+device = torch.device("cuda")
 
 def train(model, train_dl, test_dl, opt, loss_func, epochs):
     train_loss = [0 for i in range(epochs)]
@@ -13,18 +17,20 @@ def train(model, train_dl, test_dl, opt, loss_func, epochs):
     for epoch in range(epochs):
         model.train()
         for xb, yb in train_dl:
+            xb, yb = xb.to(device), yb.to(device)
             loss = loss_func(model(xb), yb)
             train_loss[epoch] = loss.item()
             loss.backward()
             opt.step()
             opt.zero_grad()
         with torch.no_grad():
-            losses, nums = zip(*[(loss_func(model(xb),yb).item(),len(xb)) for xb, yb in test_dl])
+            losses, nums = zip(*[(loss_func(model(xb.to(device)),yb.to(device)).item(),len(xb.to(device))) for xb, yb in test_dl])
             test_loss[epoch] = np.sum(np.multiply(losses, nums)) / np.sum(nums)
             correct = 0
             total = 0
             for data in test_dl:
                 images, labels = data
+                images, labels = images.to(device), labels.to(device)
                 outputs = model(images)
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
@@ -33,21 +39,20 @@ def train(model, train_dl, test_dl, opt, loss_func, epochs):
     return train_loss, test_loss
 
 if __name__ == "__main__":
-    # Select device to train on
-    device = torch.device("cuda")
     # Define transforms
     transform = transforms.Compose([torchvision.transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     # Load training data
-    train_data = torchvision.datasets.CIFAR10(root='./data/',train=True,download=True,transform=transform)
-    train_dl = torch.utils.data.DataLoader(train_data, batch_size=16, shuffle=True, num_workers=4)
-    test_data = torchvision.datasets.CIFAR10(root='./data/',train=False,download=True,transform=transform)
-    test_dl = torch.utils.data.DataLoader(test_data, batch_size=16, shuffle=True, num_workers=4)
-    
-    epochs = 100
-    model = AlexNet()
+    dataset = datasets.ImageFolder('./data', transform=transform) 
+    test_data, train_data = random_split(dataset,(4396,1099), generator=torch.Generator().manual_seed(42))
+    train_dl = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True, num_workers=4)
+    test_dl = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=True, num_workers=4)
+    # Train Model
+    epochs = 10
+    model = AlexNet().to(device)
     loss_func = nn.CrossEntropyLoss()
-    opt = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    opt = optim.Adam(model.parameters(), lr=0.0001)
     train_loss, test_loss = train(model, train_dl, test_dl, opt, loss_func, epochs)
+    # Save Model to pkl file
     f = open(f'model.pkl','wb')
     pickle.dump(model,f)
     f.close()
